003f5bda8063c8960568ce6bf827adacb19c1cb0
artem <abramyan.ar.ar@gmail.com>
2023-12-27 20:48:03 +0300

    add page loader, add not found page and error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,14 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
 import Navbar from "widgets/Navbar/ui/Navbar";
 import { Sidebar } from "widgets/Sidebar";
+import Loader from "shared/ui/Loader/Loader";
 
 function App() {
     const { theme } = useTheme();
 
     return (
         <div className={classNames("app", {}, [theme])}>
-            <Suspense fallback="Loading...">
+            <Suspense fallback={<Loader /}>
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
